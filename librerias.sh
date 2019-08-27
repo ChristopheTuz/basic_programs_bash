cef9df05dd54a4dfef683248120837c5092e4659
Christopher Tuz <54376073+ChristopheTuz@users.noreply.github.com>
2019-08-27 17:48:19 -0500

    Update librerias.sh

--- librerias.sh
+++ librerias.sh
@@ -7,7 +7,7 @@
 # No es que existan las librerías pero se puede simular
 # algo similar
 # Esta es la forma de importar funciones
-source libreria.sh
+source funciones.sh
 muestrapantalla 69 123 "epa"
 sumame 1337 3389 && echo "OK" || echo "Ocurrió un error"
 echo "Resultado: ${RESULTADO} $!"
